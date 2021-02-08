b5edde10bac6719f40393cc8cc62a4acdf0f766d
mariyamsej <mrakovkarl@gmail.com>
2021-02-08 14:04:03 +0600

    some changes

--- src/main/scala/1.scala
+++ src/main/scala/1.scala
@@ -1,32 +1,40 @@
 object Hello extends App {
 
   println("Hello")
+  println(1 + 1)
 
   def e1(): Unit = {
     val x = 1
     var y = 1
     y = y * 3
 
-    println(x)
+    println("x = " + x)
     println(y + 5)
   }
 
   def e2(x: Double, y: Int): Double = x + y
-  println(e2(1.05345, 2))
+  println("e2: " + e2(1.05345, 2))
 
   def getSquareString(input: Double): String = {
     val square = input * input
     square.toString
   }
-  println(getSquareString(2.5))
+  println("e3: " + getSquareString(2.5))
   e1()
 
   def e4(a: Int, b: Int): Int = {
     val x = if (a < b) a else b + 1
     x
   }
-  println(e4(10,  3))
+  println("e4: " + e4(10,  3))
 
-  def e5
+  def e5(x: Int): String ={
+    x match {
+      case 1 => "one"
+      case 2 => "two"
+      case _ => "neither 1 nor 2"
+    }
+  }
+  println("e5: " + e5(10))
 }
 
