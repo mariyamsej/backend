ac648e1d383188132bf141ae6181966e96b79650
mariyamsej <mrakovkarl@gmail.com>
2021-02-18 23:34:24 +0600

    +

--- src/main/scala/weer3.scala
+++ src/main/scala/weer3.scala
@@ -117,4 +117,48 @@ object week3 extends App{
     ans
   }
   println(todecimal(List(1, 0, 1, 1)))
+
+  def weakest(mat: Array[Array[Int]], k: Int): Array[Int] = {
+    var ans: Array[Int] = Array.fill[Int](k)(0)
+    var cnt: Int = 0
+    var j: Int = 0
+    var mp: scala.collection.mutable.Map[Int, Array[Int]] = scala.collection.mutable.Map[Int, Array[Int]]()
+
+    for (i <- 0 until mat.length) {
+      mp(i) = mat(i)
+    }
+
+    breakable {
+      while (j != mat(0).length) {
+        for (i <- mp.keys) {
+          if (mp(i)(j) == 0) {
+            ans(cnt) = i
+            cnt += 1
+            mp remove i
+
+            if (cnt == k) {
+              break
+            }
+          }
+        }
+        j += 1
+        if (cnt == k) {
+          break
+        }
+      }
+    }
+    breakable {
+      for (i <- mp.keys) {
+        if (cnt != k) {
+          ans(cnt) = i
+          mp remove i
+          cnt += 1
+        }
+        else {
+          break
+        }
+      }
+    }
+    ans
+  }
 }
